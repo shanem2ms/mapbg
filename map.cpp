cf499f69973be2f526f22606504071f8082f91d5
Shane Morrison <shanemor@gmail.com>
2021-06-14 20:34:25 -0400

    from script

---
+++ map.cpp
@@ -0,0 +1,215 @@
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+//
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include "map.h"
+#include <chrono>
+#include <stdio.h>
+#include "shellscalingapi.h"
+#define NANOVG_D3D11_IMPLEMENTATION
+#define COBJMACROS // This example is in c, so we use the COM macros
+#define INITGUID
+#include <Windows.h>
+#include <windowsx.h>
+
+
+
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MAP, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAP));
+
+    MSG msg;
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAP));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MAP);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+void Tick();
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+
+LARGE_INTEGER frequency;        // ticks per second
+float timePeriod;
+LARGE_INTEGER startTime;
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Store instance handle in our global variable
+
+    int pro12maxW = 1284;
+    int pro12maxH = 2778;
+    SetProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE);
+    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, pro12maxW, pro12maxH, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    SetTimer(hWnd, 0, 17, nullptr);
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&startTime);
+
+    timePeriod = 1.0f / frequency.QuadPart;
+    return TRUE;
+}
+
+int blowup = 0;
+int screenshot = 0;
+int premult = 0;
+int xm = 0;
+int ym = 0;
+int xWin = 0;
+int yWin = 0;
+
+//#define DEMO_MSAA
+struct NVGcontext* vg = NULL;
+float prevt = 0, cpuTime = 0;
+
+BOOL InitializeDX(HWND hWnd, unsigned int x, unsigned int y);
+void UnInitializeDX();
+
+const char* pszWindowClass = "WindowClass";
+
+
+static float tick = 0;
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_TIMER:
+    case WM_PAINT:
+    {
+        Tick();
+    }
+    break;
+    case WM_SIZE:
+    {
+        RECT rect;
+        GetWindowRect(hWnd, &rect);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_LBUTTONDOWN:
+        break;
+    case WM_LBUTTONUP:
+        break;
+    case WM_MOUSEMOVE:
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+void Tick()
+{
+    LARGE_INTEGER cur;
+    QueryPerformanceCounter(&cur);
+
+    float elapsedTime = (float)(cur.QuadPart - startTime.QuadPart) * timePeriod;
+}
+
+
