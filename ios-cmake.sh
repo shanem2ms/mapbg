d5d76726a1206acf760a0a2bae665b7bcfd0a727
Shane Morrison <shanemor@gmail.com>
2021-07-06 15:29:50 -0400

    from script

--- ios-cmake.sh
+++ ios-cmake.sh
@@ -1,9 +1,10 @@
+cdir=$(pwd)
 git clone --recurse-submodules https://github.com/bkaradzic/bgfx.cmake.git 
 cd bgfx.cmake
 mkdir bld
 cd bld
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_ARC=FALSE
-cd app
+cd $cdir/app
 mkdir bld
 cd bld
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64
