25ef71610bae2f14c979762ca43ad47e67729742
Shane Morrison <shanemor@gmail.com>
2021-07-07 21:34:18 -0400

    from script

--- map.cpp
+++ map.cpp
@@ -13,8 +13,7 @@
 #include <bx/thread.h>
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
-#include "logo.h"
-#include "Application.h"
+#include "core/Application.h"
 
 
 
@@ -254,22 +253,4 @@ void Tick()
 
     DrawContext ctx;
     app.Draw(ctx);
-
-#if 0
-
-    // Use debug font to print information about this example.
-    bgfx::dbgTextClear();
-    bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(width / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
-    bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-    bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-    bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-    bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-    const bgfx::Stats* stats = bgfx::getStats();
-    bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-    // Enable stats or debug text.
-    bool s_showStats = false;
-    bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
-    // Advance to next frame. Process submitted rendering primitives.
-    bgfx::frame();
-#endif
 }
